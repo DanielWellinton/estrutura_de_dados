a791f474aa4c48ddad32a10d0864c6bf4afd1c33
Daniel Wellinton Bortolini Bortolossi <403180@unoesc.lan>
2025-08-20 20:19:19 -0300

    Atividade_2

--- Atividade_01/Arrays/obj/Debug/net9.0/Arrays.AssemblyInfo.cs
+++ Atividade_01/Arrays/obj/Debug/net9.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c367ee75eb7458d5c0e4a2036826ecaa961912")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Atividade_02/Recursividade/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter the length of the Fibonacci Series: ");
+int length = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < length; i++)
+{
+    Console.Write("{0} ", FibonacciSeries(i));
+}
+Console.ReadKey();
+
+static int FibonacciSeries(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+}
