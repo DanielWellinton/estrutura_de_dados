d038969521b8100d6756fd84e0d58cfd82bf96b3
DanielWellinton <dwbb04@gmail.com>
2025-08-25 20:31:15 -0300

    Jogo da Velha

--- Atividade_01/JogoDaVelha/obj/Debug/net9.0/JogoDaVelha.AssemblyInfo.cs
+++ Atividade_01/JogoDaVelha/obj/Debug/net9.0/JogoDaVelha.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("JogoDaVelha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e765a4d97ab1ce973320ae416957e59f4f1014ee")]
-[assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c367ee75eb7458d5c0e4a2036826ecaa961912")]
+[assembly: System.Reflection.AssemblyProductAttribute("JogoDaVelha")]
+[assembly: System.Reflection.AssemblyTitleAttribute("JogoDaVelha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
