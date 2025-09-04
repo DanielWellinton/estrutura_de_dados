32572ce662144f5ffafb8a4113ea32e1c55bf672
DanielWellinton <dwbb04@gmail.com>
2025-09-03 21:56:27 -0300

    Pilhas

---
+++ Atividade_04/Pilhas/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Invertendo palavras com pilhas");
+
+Stack<char> chars = new Stack<char>();
+Console.WriteLine("Digite uma palavra: ");
+string? palavra = Console.ReadLine();
+string? palavra_invertida = null;
+
+foreach (var c in palavra)
+{
+    chars.Push(c);
+}
+while (chars.Count > 0)
+{
+    palavra_invertida += chars.Pop();
+}
+if (palavra_invertida.ToLower() == palavra.ToLower())
+{
+    Console.WriteLine("É um palíndromo");
+}
+Console.WriteLine(palavra_invertida);
+Console.WriteLine("");
