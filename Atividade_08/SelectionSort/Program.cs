48b34cbe43a803f7d121520d2e667c665a17e500
DanielWellinton <dwbb04@gmail.com>
2025-10-19 10:59:53 -0300

    Aulas atrasadas de postagens

---
+++ Atividade_08/SelectionSort/Program.cs
@@ -0,0 +1,17 @@
+﻿string esp = "************************************";
+int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+Console.WriteLine("Vetor de inteiros");
+SelectionSort.SelectionSort.Sort(integerValues);
+Console.WriteLine(string.Join(",", integerValues));
+Console.WriteLine(esp);
+
+string[] stringValues = {
+    "Bolsonaro", "Kléber", "Putin", "Pica-Pau",
+    "Matuê", "Barack Hussein Obama", "Taís Carla"
+};
+
+Console.WriteLine("Vetor de nomes");
+SelectionSort.SelectionSort.Sort(stringValues);
+Console.WriteLine(string.Join(",", stringValues));
+Console.WriteLine(esp);
