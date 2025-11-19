3107e8e5e4edcc15fb6715ec7acfdcf33c45eac6
DanielWellinton <dwbb04@gmail.com>
2025-11-19 20:27:53 -0300

    Desafio Algoritmo

---
+++ Atividade_10/DesafioAlgoritmo/Program.cs
@@ -0,0 +1,27 @@
+﻿List<string> lista = [
+    "Dhomini", "Bernardo", "Luiz", "Kauany",
+    "Alisson", "Henrique", "Luyze", "Isaias",
+    "Daniel", "Leonardo"
+    ];
+
+Console.WriteLine("Digite um nome: ");
+string? nome = Console.ReadLine();
+bool encontrado = false;
+
+if (nome != null)
+{
+    for (int i = 0; i < lista.Count(); i++)
+    {
+        if (nome.ToUpper().CompareTo(lista[i].ToUpper()) == 0)
+        {
+            Console.WriteLine("Nome encontra-se na posição: " + i);
+            encontrado = true;
+            break;
+        }
+    }
+}
+
+if (!encontrado)
+{
+    Console.WriteLine("Não encontrado");
+}
